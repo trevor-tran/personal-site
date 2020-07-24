4694a9f1e5213fd37ec58b1ddd83f026bdf8cf32
Phuong Tran <ptran816@uw.edu>
2020-07-24 01:30:43 -0700

    put technical skills on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,32 +4,64 @@ function Home() {
   const handleClick = () => {
     window.open("Phuong-Tran-Resume.pdf", "_blank");
   }
+
+  const SKILLS = [
+    { skill: "Java", proficency: "60%" },
+    { skill: "JavaScript", proficency: "60%" },
+    { skill: "ReactJS", proficency: "80%" },
+    { skill: "HTML/CSS", proficency: "60%" },
+    { skill: "SQL", proficency: "60%" }
+  ];
+
+  // "skills" is a list of objects following the format below:
+  // [{skill: "React", proficency: "50%"},{skill: "Java", proficency: "60%"}]
+  const makeSkillList = (skills) => {
+    const COLS = 2;
+    let lst = [];
+    let row = [];
+    let elementPerRow = 0;
+    skills.forEach((s, i) => {
+      if (elementPerRow === COLS) {
+        lst.push(<tr key={i}>{row}</tr>);
+        //reset
+        row = [];
+        elementPerRow = 0;
+      }
+      row.push(
+        <td key={s.skill}>
+          <h6>{s.skill}</h6>
+          <div className="progress">
+            <div className="progress-bar"
+              role="progressbar"
+              aria-valuenow={s.proficency}
+              aria-valuemin="0%"
+              aria-valuemax="100%"
+              style={{ width: s.proficency }}
+            />
+          </div>
+        </td>
+      );
+      elementPerRow++;
+    });
+    if (row.length > 0) {
+      lst.push(<tr key={"0"}>{row}</tr>);
+    }
+    return lst;
+  }
+
   return (
     <React.Fragment>
       <div className="intro">
         <h6>Hello, my name is</h6>
-        <h1 style={{ width: "300px" }}><b>Phuong D. Tran</b></h1>
+        <h1 style={{ width: "290px" }}><b>Phuong D. Tran</b></h1>
         <p className="lead">I am a new grad in Computer Science at University of Washington, Seattle, Washington.</p>
         <button type="button" className="btn btn-outline-dark" onClick={handleClick}>View my resume</button>
       </div>
       <div className="skills">
-        <h2>My skills</h2>
-        <table style={{ width: "100%" }}>
+        <h2>Technical skills</h2>
+        <table>
           <tbody>
-            <tr>
-              <td>
-                <h6>ReactJS</h6>
-                <div className="progress">
-                  <div className="progress-bar"
-                    role="progressbar"
-                    aria-valuenow="40"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                    style={{ width: "40%" }}
-                  />
-                </div>
-              </td>
-            </tr>
+            {makeSkillList(SKILLS)}
           </tbody>
         </table>
       </div>
