8059a29e834d13f177f8a00be1138c0ce5cd802c
Phuong Tran <ptran816@uw.edu>
2020-07-31 00:54:27 -0700

    smooth box transition, shadow-none for inputs

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -7,20 +7,20 @@ function MessageBox() {
     <form className="message-box">
       <div className="row">
         <div className="col">
-          <input type="text" className="form-control" placeholder="Name" />
+          <input type="text" className="form-control shadow-none" placeholder="Name" />
         </div>
         <div className="col">
-          <input type="text" className="form-control" placeholder="Email" />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <input type="text" className="form-control" placeholder="Subject" />
+          <input type="text" className="form-control shadow-none" placeholder="Email" />
         </div>
       </div>
       <div className="row">
         <div className="col">
-          <textarea type="text" className="form-control" placeholder="Message" />
+          <input type="text" className="form-control shadow-none" placeholder="Subject" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <textarea type="text" className="form-control shadow-none" placeholder="Message" />
         </div>
       </div>
       <button type="submit" className="btn btn-primary mb-2 send-button">Send Message</button>
