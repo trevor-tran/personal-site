984a1c8f632285f52b77fd5bb6c413c41ff2827e
Phuong Tran <ptran816@uw.edu>
2020-08-01 01:13:15 -0700

    message box can send an email with EmailJS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="navbar fixed-bottom" style={{ backgroundColor: "black", borderTop: "1px solid #4b2e83" }}>
       <div className="container justify-content-center" style={{ margin: "auto", height: "40px" }}>
-        <p> Modified July 23, 2020</p>
+        <p> Modified August 8, 2020</p>
       </div>
     </footer>
   );
