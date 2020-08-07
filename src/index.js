a97cd5547dad9b68c8c8ce2a93d46b746556efc5
Phuong Tran <ptran816@uw.edu>
2020-08-06 22:02:31 -0700

    removed css code in App, serviceWorker deleted, changed margin adapting small devices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
@@ -13,7 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
