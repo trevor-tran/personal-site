0b0da7da294a7ba60dbc4acfd3810c7037a2e804
Phuong Tran <ptran816@uw.edu>
2020-08-07 00:37:43 -0700

    sidenav added for small screen devices, commented right icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,56 @@
 import React from 'react';
-// import "../css/Header.css"
+import $ from 'jquery';
+import MediaQuery from 'react-responsive';
+
 import '../css/Header.css';
 
 function Header() {
+
+  const openSideNav = () => {
+    console.log("opened")
+    $("#sidenav-id").width("200px");
+  }
+
+  const closeSideNav = () => {
+    console.log("opened")
+    $("#sidenav-id").width("0px");
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top">
-      <div className="navbar-collapse justify-content-center order-2">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/aboutme/">About Me</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/portfolio/">Portfolio</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-collapse collapse justify-content-center order-2">
+      <MediaQuery minWidth={1224}>
+        <div className="navbar-collapse justify-content-center order-2">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/aboutme/">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/portfolio/">Portfolio</a>
+            </li>
+          </ul>
+        </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={1224}>
+        <div className="sidenav-container">
+          <div id="sidenav-id" className="sidenav">
+            <button className="closebtn" onClick={closeSideNav}>&times;</button>
+              <a href="/">Home</a>
+              <a href="/aboutme/">About Me</a>
+              <a  href="/portfolio/">Portfolio</a>
+          </div>
+          <nav className="navbar navbar-dark">
+            <button className="navbar-toggler" type="button">
+              <span className="navbar-toggler-icon" onClick={openSideNav}></span>
+            </button>
+          </nav>
+        </div>
+      </MediaQuery>
+
+      {/* icons on the right */}
+      {/* <div className="navbar-collapse justify-content-center order-2">
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link" href="https://github.com/PhuongDTran" target="_blank" rel="noopener noreferrer">
@@ -36,7 +68,7 @@ function Header() {
             </a>
           </li>
         </ul>
-      </div>
+      </div> */}
     </nav>
   );
 }
