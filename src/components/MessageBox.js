640416f587a70dbd3594dcaa93b89d6e5d72ba39
Phuong Tran <ptran816@uw.edu>
2020-08-07 22:15:52 -0700

    clearing out inputs on success

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import '../css/MessageBox.css';
 import emailjs from 'emailjs-com';
 
@@ -8,13 +8,15 @@ function MessageBox() {
   const SERVICE_ID = "my_site_message_service";
   const TEMPLATE_ID = "message_template";
 
-  const fields = useRef({ user_name: "", user_email: "", subject: "", message: "" });
+  const [fields, setFields] = useState({ user_name: "", user_email: "", subject: "", message: "" });
 
   const sendEmail = e => {
     e.preventDefault();
     if (isValid()) {
       emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
         .then((result) => {
+          //clear out inputs
+          setFields({ user_name: "", user_email: "", subject: "", message: "" });
           alert(result.text);
         }, (error) => {
           alert(error.text);
@@ -25,32 +27,33 @@ function MessageBox() {
   }
 
   const isValid = () => {
-    return Object.values(fields.current).reduce((acc, cur) => !!acc & !!cur);
+    return Object.values(fields).reduce((acc, cur) => !!acc & !!cur);
   }
 
   const handleChange = (e) => {
-    fields.current[e.target.name] = e.target.value;
-    console.log(fields.current);
+    let clone = {...fields};
+    clone[e.target.name] = e.target.value;
+    setFields(clone);
   }
 
   return (
     <form className="message-box d-flex h-100 flex-column" onSubmit={sendEmail}>
       <div className="row">
         <div className="col">
-          <input type="text" className="form-control shadow-none" name="user_name" placeholder="Name" onChange={handleChange} />
+          <input type="text" className="form-control shadow-none" name="user_name" placeholder="Name" value={fields.user_name} onChange={handleChange} />
         </div>
         <div className="col">
-          <input type="text" className="form-control shadow-none" name="user_email" placeholder="Email" onChange={handleChange} />
+          <input type="text" className="form-control shadow-none" name="user_email" placeholder="Email" value={fields.user_email}  onChange={handleChange} />
         </div>
       </div>
       <div className="row">
         <div className="col">
-          <input type="text" className="form-control shadow-none" name="subject" placeholder="Subject" onChange={handleChange} />
+          <input type="text" className="form-control shadow-none" name="subject" placeholder="Subject" value={fields.subject} onChange={handleChange} />
         </div>
       </div>
       <div className="row flex-fill">
         <div className="col">
-          <textarea type="text" className="form-control shadow-none message-input" name="message" placeholder="Message" onChange={handleChange} />
+          <textarea type="text" className="form-control shadow-none message-input" name="message" placeholder="Message" value={fields.message} onChange={handleChange} />
         </div>
       </div>
       <button type="submit" className="btn btn-primary shadow-none send-button">Send Message</button>
