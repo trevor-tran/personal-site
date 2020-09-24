ba4ee3fb191010fabe1008769352f56e79f8b30e
Phuong Tran <ptran816@uw.edu>
2020-09-24 12:09:29 -0700

    updated resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="navbar fixed-bottom">
       <div className="container justify-content-center">
-        <p> Modified August 7, 2020</p>
+        <p> Modified Sep. 10, 2020</p>
       </div>
     </footer>
   );
